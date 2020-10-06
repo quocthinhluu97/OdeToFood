84b3b0d626061c5040f221957a9318332f52a432
Thinh Quoc LUU <quocthinhluu97@gmail.com>
2020-10-06 14:56:00 +0700

    Add DbContext

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using OdeToFood.Core;
+﻿using OdeToFood.Core;
 using System.Collections.Generic;
 
 namespace OdeToFood.Data
@@ -8,78 +7,9 @@ namespace OdeToFood.Data
     {
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
         Restaurant GetById(int id);
+        Restaurant Remove(int id);
         Restaurant Update(Restaurant updatedRestaurant);
         Restaurant Add(Restaurant newRestaurant);
         int Commit();
-    }
-
-    public class InMemoryRestaurantData : IRestaurantData
-    {
-        readonly List<Restaurant> restaurants;
-
-        public InMemoryRestaurantData()
-        {
-            restaurants = new List<Restaurant>()
-            {
-                new Restaurant
-                {
-                    Id = 1,
-                    Name = "Scott's",
-                    Location = "MaryLand",
-                    Cuisine = CuisineType.Indian
-                },
-                new Restaurant
-                {
-                    Id = 2,
-                    Name = "Peter's",
-                    Location = "HolyLand",
-                    Cuisine = CuisineType.Italian
-                },
-                new Restaurant
-                {
-                    Id = 3,
-                    Name = "John's",
-                    Location = "Ali",
-                    Cuisine = CuisineType.Mexican
-                }
-            };
-        }
-
-        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
-        {
-            return from r in restaurants
-                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
-                   orderby r.Name
-                   select r;
-        }
-
-        public Restaurant GetById(int id)
-        {
-            return restaurants.SingleOrDefault(r=>r.Id == id);
-        }
-
-        public Restaurant Update(Restaurant updatedRestaurant)
-        {
-            var restaurant = restaurants.FirstOrDefault(r=>r.Id == updatedRestaurant.Id);
-            if (restaurant != null)
-            {
-                restaurant.Name = updatedRestaurant.Name;
-                restaurant.Location = updatedRestaurant.Location;
-                restaurant.Cuisine = updatedRestaurant.Cuisine;
-            }
-            return restaurant;
-        }
-
-        public int Commit()
-        {
-            return 0;
-        }
-
-        public Restaurant Add(Restaurant newRestaurant)
-        {
-            restaurants.Add(newRestaurant);
-            newRestaurant.Id = restaurants.Max(r=>r.Id) + 1;
-            return newRestaurant;
-        }
     }
 }
--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,7 +20,11 @@ namespace OdeToFood
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
+            services.AddDbContextPool<OdeToFoodDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("OdeToFoodDb"));
+            });
+            services.AddScoped<IRestaurantData, SqlRestaurantData>();
             services.AddRazorPages();
         }
 
